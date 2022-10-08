e410bb579f5663f0b8b63da7ba312280950ddebf
TheoParamelle <72016649+TheoParamelle@users.noreply.github.com>
2022-10-08 21:40:40 +0200

    update url

--- pages/index.js
+++ pages/index.js
@@ -354,7 +354,7 @@ export default function Home({ project }) {
           <ul className="footer-links">
             <li>
               <motion.a
-                href="https://drive.google.com/file/d/1PgvpHThs5XjTwGZgib9ZTVLa8QbhulWp/view?usp=sharing"
+                href="https://drive.google.com/file/d/1g8cgMF3c1IP18-GT_qQRoyFL6_8jQCIi/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.1 }}
@@ -366,7 +366,7 @@ export default function Home({ project }) {
             </li>
             <li>
               <motion.a
-                href="https://github.com/GameDog9988"
+                href="https://github.com/TheoParamelle"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.05 }}
@@ -379,7 +379,7 @@ export default function Home({ project }) {
             </li>
             <li>
               <motion.a
-                href="https://www.linkedin.com/in/alexander-grattan/"
+                href="www.linkedin.com/in/théo-paramelle"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.1 }}
