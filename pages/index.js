160c1b46dc4cfa5c6d4d4bc647f99508ccb32204
TheoParamelle <72016649+TheoParamelle@users.noreply.github.com>
2022-10-08 21:33:51 +0200

    update resume

--- pages/index.js
+++ pages/index.js
@@ -207,7 +207,7 @@ export default function Home({ project }) {
               <ul className="nav-list">
                 <li>
                   <motion.a
-                    href="https://drive.google.com/file/d/1PgvpHThs5XjTwGZgib9ZTVLa8QbhulWp/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1g8cgMF3c1IP18-GT_qQRoyFL6_8jQCIi/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                     whileHover={{ scale: 1.1 }}
