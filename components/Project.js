d749ba582c0c89f9bb19527fdcba4d40bcaa4f33
Latets <72016649+TheoParamelle@users.noreply.github.com>
2024-06-13 03:16:50 +0200

    Update Project.js

--- components/Project.js
+++ components/Project.js
@@ -41,7 +41,7 @@ export default function Project({ children, pageTitle, description }) {
           <ul className="nav-list">
             <li>
               <motion.a
-                href="https://drive.google.com/file/d/1eREDluqf4DrxUfmo_arw0uEP4ni3ztJQ/view?usp=sharing"
+                href="https://drive.google.com/file/d/1gqdLMGN46WzIf78mmtQ_PcfOQRsPPCIB/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.1 }}
